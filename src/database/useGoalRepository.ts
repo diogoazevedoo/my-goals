3993bf259061e9594390ea896ffc570ce8ea044a
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-06 17:04:32 +0000

    Feat: created function to show goal details based on the goal clicked

--- src/database/useGoalRepository.ts
+++ src/database/useGoalRepository.ts
@@ -43,8 +43,23 @@ export function useGoalRepository() {
         }
     }
 
+    function show(id: number) {
+        const statement = database.prepareSync(`
+            SELECT g.id, g.name, g.total, COALESCE(SUM(t.amount), 0) AS current
+            FROM goals AS g
+            LEFT JOIN transactions t ON t.goal_id = g.id
+            WHERE g.id = $id
+            GROUP BY g.id, g.name, g.total;
+        `)
+
+        const result = statement.executeSync<GoalResponseDatabase>({ $id: id })
+
+        return result.getFirstSync()
+    }
+
     return {
         create,
         all,
+        show,
     }
 }
